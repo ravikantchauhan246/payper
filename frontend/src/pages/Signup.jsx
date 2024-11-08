573e4683a28d1f304ed5cf64d4a3cc7498dd5fd9
Ravikant Chauhan <53376708+ravikantchauhan246@users.noreply.github.com>
2024-11-08 17:04:38 +0530

    add frontend integration

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,30 +1,82 @@
-import { Button } from "../components/Button"
-import { ButtonWarning } from "../components/ButtonWarning"
-import { Heading } from "../components/Heading"
-import { InputBox } from "../components/InputBox"
-import { SubHeading } from "../components/SubHeading"
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { ButtonWarning } from "../components/ButtonWarning";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
-    return(
-        <div className="bg-slate-300 h-screen flex justify-center">
-            <div className="flex flex-col justify-center">
-                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-                    <Heading label={"Sign up"}></Heading>
-                    <SubHeading label={"Enter your information to create an account"}></SubHeading>
-                    <InputBox label={"First Name"} placeholder={"John"}></InputBox>
-                    <InputBox label={"Last Name"} placeholder={"Doe"}></InputBox>
-                    <InputBox label={"Email"} placeholder={"johndoe@gmail.com"}></InputBox>
-                    <InputBox label={"Password"} placeholder={"john@doe"}></InputBox>
-                    
-                    <div className="pt-4">
-                        <Button label={"Sign up"}></Button>
-                    </div>
+  const navigate = useNavigate()
 
-                    <ButtonWarning label={"Already have an account?"} linkText={"Sign in"} to={"/signin"}></ButtonWarning>
-                </div>
-            </div>
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign up"}></Heading>
+          <SubHeading
+            label={"Enter your information to create an account"}
+          ></SubHeading>
+          <InputBox
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            label={"First Name"}
+            placeholder={"John"}
+          ></InputBox>
+          <InputBox
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            label={"Last Name"}
+            placeholder={"Doe"}
+          ></InputBox>
+          <InputBox
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            label={"Email"}
+            placeholder={"johndoe@gmail.com"}
+          ></InputBox>
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            label={"Password"}
+            placeholder={"john@doe"}
+          ></InputBox>
 
+          <div className="pt-4">
+            <Button
+              onClick={async() => {
+                const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+                  username,
+                  firstName,
+                  lastName,
+                  password,
+                })
+
+                localStorage.setItem("token",response.data.token)
+                navigate("/dashboard")
+
+              }}
+              label={"Sign up"}
+            ></Button>
+          </div>
+
+          <ButtonWarning
+            label={"Already have an account?"}
+            linkText={"Sign in"}
+            to={"/signin"}
+          ></ButtonWarning>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
